f892b23bf406f565bd2ad9324b607bdc5edfd29b
RJOlmedo <88732876+a00829748@users.noreply.github.com>
2021-10-27 12:21:11 -0500

    Se agrego funcion para dibujar circulo y nombres

--- Paint.py
+++ Paint.py
@@ -42,18 +42,55 @@ def square(start, end):
 
 
 def circle2(start, end):
-    "Draw circle from start to end."
-    pass  # TODO
-
-
-def rectangle(start, end):
-    "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    r = (end.x - start.x)/2
+    circle(r)
+    end_fill()  # TODO
 
 
 def triangle(start, end):
-    "Draw triangle from start to end."
-    pass  # TODO
+    #sube el lapiz
+    up()
+    goto(start.x, start.y)
+    #se baja el lapiz para dibujar
+    down()
+    #iniciar el fill para rellenar
+    begin_fill()
+    
+    #se calcula la distancia entre cada vertice del triangulo
+    forward(end.x - start.x)
+    for count in range(2):
+        #Gira para poder cerrar el triángulo
+        left(120)
+        #el tramo de vértice a vértice
+        forward(end.x - start.x)
+    
+    end_fill()
+
+
+
+def triangle(start, end):
+    #sube el lapiz
+    up()
+    goto(start.x, start.y)
+    #se baja el lapiz para dibujar
+    down()
+    #iniciar el fill para rellenar
+    begin_fill()
+    
+    #se calcula la distancia entre cada vertice del triangulo
+    forward(end.x - start.x)
+    for count in range(2):
+        #Gira para poder cerrar el triángulo
+        left(120)
+        #el tramo de vértice a vértice
+        forward(end.x - start.x)
+    
+    end_fill()
+
 
 
 def tap(x, y):
@@ -74,6 +111,15 @@ def store(key, value):
     "Store value in state at key."
     state[key] = value
 
+def info_alumnos():
+    writer.up()
+    writer.goto(0, 190)
+    writer.color('red')
+    writer.write("Samuel Acosta")
+    writer.goto(0, 170)
+    writer.color('red')
+    writer.write("Ricardo Olmedo")
+
 
 #indica lo que va a dibujar - a funcion que se va a ejecutar
 #shape indica lo que se va a dibujar
@@ -86,17 +132,22 @@ print(type(state))
 setup(width=420, height=420, startx=0, starty=0)
 onscreenclick(tap)
 listen()
+writer = Turtle(visible=False)
 #onkey necesita una fx sin argumentos por eso se uso la lambda - funcion anonima sin argumentos
 #los colores pueden usarse hexadecimales que los obtenemos de internet
+info_alumnos()
 onkey(undo, 'u')
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('B350FF'), 'P') # P de purple porque el color es morado
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle2), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
+
+#onkey(lambda: store('shape', info_alumnos), 'a')
 done()
